dbddc08159e647446a985b7e196a6d3ce94008f2
user <userer>
2020-01-15 17:31:07 +0700

    1

---
+++ src/armyani.kt
@@ -0,0 +1,41 @@
+class Mat{
+    var first : Float = 0f
+    var second: Float = 0f
+    var resX: Int = 1920
+    var rexY: Int = 1080
+    fun changex() {
+            var firstU = readLine()!!.toFloat()
+            if (firstU < 1080 && firstU > 0) {
+                first = firstU
+            }
+            else {
+                println("Выход за пределы по X")
+            }
+        }
+
+    fun changey() {
+            var secondU = readLine()!!.toFloat()
+            if (secondU < 1080 && secondU > 0) {
+                second = secondU
+            }
+            else {
+                println("Выход за пределы по Y")
+            }
+        }
+
+    fun display() {
+        println("$first $second")
+    }
+
+}
+fun makeC(/*first: Float, second: Float*/): Mat{
+    var a = Mat()
+   /* a.first = first
+    a.second = second */
+    a.changex()
+    a.changey()
+    return a
+}
+fun main(){
+    makeC().display()
+}
